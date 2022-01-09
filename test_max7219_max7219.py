f99ebc784b9db7377e27ac91065fe253e3e8e53e
TehTing <67617687+TehTing@users.noreply.github.com>
2022-01-09 23:27:08 +0800

    Update and rename max7219_test_max7219.py to test_max7219_max7219.py

--- test_max7219_max7219.py
+++ test_max7219_max7219.py
@@ -1,3 +1,11 @@
+'''
+用 max7219 套件測試裝置 max7219 是否能夠使用
+原本此程式測試後可以正常運作
+但可能因為文件更新的關係，這幾天再測試時是失敗的
+建議改用 luma 套件控制 max7219
+測試程式碼請換用 test_max7219_luma.py
+'''
+
 import max7219.led as led
 import max7219.canvas as canvas
 import max7219.transitions as transitions
